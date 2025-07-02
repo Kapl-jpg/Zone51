fc1f4641554fb0b58e1e272f1b5faae09c632d76
Kapl-jpg <ivan_titov_2000@bk.ru>
2025-07-02 15:29:59 +0300

    Revert "Merge branch 'Dmitriy22' into Ivan22"
    This reverts commit 5445db3f010277a395575eb4089ad7493f6cda02, reversing
    changes made to 6f221c0caadceff8091612a35910b909e9682781.

---
+++ Assets/Scripts/Menu/PauseController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField] private InputMeneger inputMeneger;
+    [SerializeField] private GameObject menuPause;
+
+    private bool _activeMenuPause;
+
+    private void Update()
+    {
+        if (inputMeneger.InputPause())
+        {
+            PauseMode();
+        }
+    }
+
+    [Event("PauseMode")]
+
+    private void PauseMode()
+    {
+        _activeMenuPause = !_activeMenuPause;
+
+        if (_activeMenuPause)
+        {
+            menuPause.SetActive(true);
+            EventManager.Publish("OnOffCursor", true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            menuPause.SetActive(false);
+            EventManager.Publish("OnOffCursor", false);
+            Time.timeScale = 1;
+        }
+    }
+}
