cd59325df020ae4c135a12d71bd449fa29c87c0c
Kapl-jpg <ivan_titov_2000@bk.ru>
2025-07-04 10:16:05 +0300

    Created classics, tested human animations

---
+++ Assets/Scripts/Classic/Plate.cs
@@ -0,0 +1,42 @@
+using Enums;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    [SerializeField] private Plate nextPlate;
+    [SerializeField] private CharacterType plateType;
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool active;
+
+    private bool _activated;
+    private bool _canUse;
+    
+    private void Start()
+    {
+        _canUse = active;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if(!_canUse || _activated) return;
+        
+        if (!other.gameObject.CompareTag("Player")) return;
+        
+        var characterType = RequestManager.GetValue<CharacterType>("CharacterType");
+        
+        if (characterType != plateType) return;
+
+        if(nextPlate)
+            nextPlate._canUse = true;
+        
+        _canUse = false;
+        print(plateType);
+    }
+
+    [Event("ResetPlate")]
+    private void OnResetPlate()
+    {
+        _canUse = active;
+        _activated = false;
+    }
+}
