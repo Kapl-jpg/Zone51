d85ffe958ae0d0e9da4a3f5938f6c6c9ec32b0bd
Kapl <ivan_titov_2000@bk.ru>
2025-07-06 14:46:48 +0300

    Fixed crouching

--- Assets/Scripts/Player/PlayerCrouch.cs
+++ Assets/Scripts/Player/PlayerCrouch.cs
@@ -5,40 +5,39 @@ namespace Player
 {
     public class PlayerCrouch : Subscriber
     {
-        [SerializeField] private GameObject crouchBody;
+        [SerializeField] private CapsuleCollider alienCollider;
         [SerializeField] private InputMeneger input;
 
-        private Vector3 _scale;
-        private bool _isCrouching;
+        private Vector3 _defaultCenter;
+        private float _defaultHeight;
+        
+        private Vector3 _crouchCenter;
+        private float _crouchHeight;
 
         private void Start()
         {
-            _scale = crouchBody.transform.localScale;
+            _defaultCenter = alienCollider.center;
+            _defaultHeight = alienCollider.height;
+            
+            _crouchCenter = alienCollider.center - Vector3.up * 0.25f;
+            _crouchHeight = alienCollider.height * 0.7f;
         }
 
         private void Update()
         {
+            var characterType = RequestManager.GetValue<CharacterType>("CharacterType");
+            if (characterType == CharacterType.Human) return;
+            
             if (input.Crouch())
             {
-                var characterType = RequestManager.GetValue<CharacterType>("CharacterType");
-
-                if (characterType == CharacterType.Human) return;
-
-                if (_isCrouching) return;
-                
-                crouchBody.transform.localScale = new Vector3(_scale.x, _scale.y/2, _scale.z);
-                
-                _isCrouching = true;
+                alienCollider.center = _crouchCenter;
+                alienCollider.height = _crouchHeight;
                 EventManager.Publish("Crouch", true);
             }
             else
             {
-                if (!_isCrouching) return;
-                
-                
-                crouchBody.transform.localScale = new Vector3(_scale.x, _scale.y, _scale.z);
-
-                _isCrouching = false;
+                alienCollider.center = _defaultCenter;
+                alienCollider.height = _defaultHeight;
                 EventManager.Publish("Crouch", false);
             }
         }
