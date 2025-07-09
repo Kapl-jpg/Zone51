d405265621dd3a1df74b75a471cfdba42bdfcb1d
Kapl <ivan_titov_2000@bk.ru>
2025-07-09 19:04:01 +0300

    Fixed build bugs

--- Assets/Scripts/Cutscene/LoadGameScene.cs
+++ Assets/Scripts/Cutscene/LoadGameScene.cs
@@ -1,14 +1,11 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadGameScene : Subscriber
 {
-    [SerializeField] private SceneAsset scene;
-
     [Event("StartGame")]
     public void LoadScene()
     {
-        SceneManager.LoadScene(scene.name);
+        SceneManager.LoadScene(2);
     }
 }
