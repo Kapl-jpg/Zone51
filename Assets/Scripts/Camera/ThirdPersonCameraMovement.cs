53d4dfb70357c14f39426e93b5f3261b1f3a6f16
Kapl <ivan_titov_2000@bk.ru>
2025-07-13 22:20:37 +0300

    Added borders for third person camera

--- Assets/Scripts/Camera/ThirdPersonCameraMovement.cs
+++ Assets/Scripts/Camera/ThirdPersonCameraMovement.cs
@@ -7,10 +7,16 @@ public class ThirdPersonCameraMovement : MonoBehaviour
     [SerializeField] private CinemachineOrbitalFollow orbitalFollow;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private MouseManager mouseManager;
+    [SerializeField] private Vector2 verticalBorders;
 
     private void Update()
     {
-        orbitalFollow.HorizontalAxis.Value += inputManager.InputMouse().x * mouseManager.MouseSensitivity * Time.deltaTime;
-        orbitalFollow.VerticalAxis.Value -= inputManager.InputMouse().y * mouseManager.MouseSensitivity * Time.deltaTime;
+        orbitalFollow.HorizontalAxis.Value +=
+            inputManager.InputMouse().x * mouseManager.MouseSensitivity * Time.deltaTime;
+        orbitalFollow.VerticalAxis.Value =
+            Mathf.Clamp(
+                orbitalFollow.VerticalAxis.Value -
+                inputManager.InputMouse().y * mouseManager.MouseSensitivity * Time.deltaTime, verticalBorders.x,
+                verticalBorders.y);
     }
 }
