89d0cd59cd2e195bf3fc1c039f6db15cd94b4988
Kapl <ivan_titov_2000@bk.ru>
2025-07-17 15:36:50 +0300

    Changed tutorials

---
+++ Assets/Scripts/Tutorials/AnimatorChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimatorChecker: MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private void Update()
+    {
+        if (animator.speed == 0f)
+        {
+            Debug.Log("Animator остановлен (speed = 0)");
+        }
+        else
+        {
+            Debug.Log("Animator работает (speed > 0)");
+        }
+    }
+}
