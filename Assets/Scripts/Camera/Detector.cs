0bb799d58ce1ab3c76c1fd3f7178bb472a5c434a
Kapl <ivan_titov_2000@bk.ru>
2025-07-21 22:09:12 +0300

    Revert "Changed camera effect"
    This reverts commit c231113451a43196ab1ce074de55a4633c2571e1.

--- Assets/Scripts/Camera/Detector.cs
+++ Assets/Scripts/Camera/Detector.cs
@@ -5,8 +5,8 @@ public class Detector : Subscriber
 {
     [SerializeField] private Transform rayOrigin;
     [SerializeField] private Transform sphereCenter;
-    [SerializeField] private AudioSource[] audioAlarms; // пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-    [SerializeField] private AudioSource[] audioDoctors; // пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+    [SerializeField] private AudioSource[] audioAlarms; // Массив звуков сигнализации
+    [SerializeField] private AudioSource[] audioDoctors; // Массив звуков для доктора
     [SerializeField] private LayerMask detectionLayer;
     [SerializeField] private LayerMask obstacleLayerMask;
     [SerializeField] private FlickerLight flickerLight;
@@ -147,8 +147,7 @@ public class Detector : Subscriber
     {
         if (rayOrigin == null || detectedPlayer == null) return false;
 
-        detectedPlayer.TryGetComponent(out Collider hitCollider);
-        Vector3 direction = (hitCollider.bounds.center - rayOrigin.position).normalized;
+        Vector3 direction = (detectedPlayer.position - rayOrigin.position).normalized;
         float distance = Vector3.Distance(rayOrigin.position, detectedPlayer.position);
 
         if (Physics.Raycast(rayOrigin.position, direction, out RaycastHit obstacleHit, distance, obstacleLayerMask))
