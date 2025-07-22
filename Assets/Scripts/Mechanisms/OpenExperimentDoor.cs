0dac2c0aa5d5651da3a258cb3f3a1ac5993fd6ec
Dima <dima_yevdokimov@list.ru>
2025-07-22 21:11:45 +0700

    Update Scene

--- Assets/Scripts/Mechanisms/OpenExperimentDoor.cs
+++ Assets/Scripts/Mechanisms/OpenExperimentDoor.cs
@@ -3,31 +3,43 @@ using UnityEngine;
 
 namespace Mechanisms
 {
-    public class OpenExperimentDoor: Subscriber
+    public class OpenExperimentDoor : Subscriber
     {
         private static readonly int Open = Animator.StringToHash("Open");
         private static readonly int Close = Animator.StringToHash("Close");
         [SerializeField] private Animator doorAnimator;
         [SerializeField] private AudioSource audioOpen;
         [SerializeField] private AudioSource audioClose;
+        private bool _opened;
+        private bool _isOpen = false;
 
         [Event("OpenExperimentDoor")]
         private void OpenDoor()
         {
+            //if (!_isOpen)
+            //{
+                
+            //}
+            if (!_opened) return;
             var getPower = RequestManager.GetValue<bool>("GetPower");
 
-            if (getPower)
-            {
-                doorAnimator.SetTrigger(Open);
-                audioOpen.Play();
-            }
+            if (!getPower) return;
+            doorAnimator.SetTrigger(Open);
+            audioOpen.Play();
+            _opened = true;
+            _isOpen = true;
         }
 
         [Event("CloseExperimentDoor")]
         private void CloseDoor()
         {
+            //if (_isOpen)
+            //{
+                
+            //}
             doorAnimator.SetTrigger(Close);
             audioClose.Play();
+            _isOpen = false;
         }
     }
 }
